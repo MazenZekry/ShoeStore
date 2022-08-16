defd1bd41ba3bb5aa7b2f15ac311d99411f9fc67
MaZeN <mazen.zekry6@gmail.com>
2022-08-16 18:57:09 +0200

    menuProvider instead of deprecated one

--- app/src/main/java/com/mazen/shoestore/shoe_list/ShoeListFragment.kt
+++ app/src/main/java/com/mazen/shoestore/shoe_list/ShoeListFragment.kt
@@ -2,15 +2,18 @@ package com.mazen.shoestore.shoe_list
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.mazen.shoestore.R
 import com.mazen.shoestore.databinding.FragmentShoeListBinding
 import com.mazen.shoestore.databinding.ShoeItemListBinding
 import com.mazen.shoestore.model.Shoe
 
-class ShoeListFragment : Fragment() {
+class ShoeListFragment : Fragment(), MenuProvider{
     lateinit var binding: FragmentShoeListBinding
     private val viewModel: ShoeViewModel by activityViewModels()
     override fun onCreateView(
@@ -26,29 +29,14 @@ class ShoeListFragment : Fragment() {
             }
         }
         binding.btnAddShoe.setOnClickListener {
-            addNewShoe()
+            addNewShoeDetails()
         }
-        setHasOptionsMenu(true)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
         return binding.root
     }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.logout_menu,menu)
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.loginFragment -> {
-                findNavController().navigate(R.id.loginFragment)
-                true
-            }
-            else -> false
-        }
-    }
-
-    private fun addNewShoe() {
+    private fun addNewShoeDetails() {
         findNavController().navigate(R.id.action_shoeListFragment_to_detailsFragment)
     }
 
@@ -57,5 +45,19 @@ class ShoeListFragment : Fragment() {
         shoeItemBinding.shoeItem = shoe
         binding.innerLayout.addView(shoeItemBinding.root)
 
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.logout_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.loginFragment -> {
+                findNavController().navigate(R.id.loginFragment)
+                true
+            }
+            else -> false
+        }
     }
 }
